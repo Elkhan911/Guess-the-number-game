ec4fe90a4747c5e8a19a53bc8654782e0a0b9636
Elkhan911 <955pro@mail.ru>
2022-09-03 01:58:05 +0500

    editing css code

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const computerNumber = getRandom(0, 100);
+const computerNumber = getRandom(0, 50);
 console.log(computerNumber);
 
-input1.addEventListener("blur", function () {
-  if (input1.value > computerNumber) {
-    span1.textContent = "вы ввели значние БОЛЬШЕ";
-  }
+input1.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    if (input1.value > computerNumber) {
+      span1.textContent = "вы ввели значние БОЛЬШЕ";
+    }
 
-  if (input1.value < computerNumber) {
-    span1.textContent = "вы ввели значние МЕНЬШЕ";
-  }
+    if (input1.value < computerNumber) {
+      span1.textContent = "вы ввели значние МЕНЬШЕ";
+    }
 
-  if (input1.value == computerNumber) {
-    span1.textContent = "БИНГО! Вы угадали";
+    if (input1.value == computerNumber) {
+      span1.textContent = "БИНГО! Вы угадали";
+    }
   }
 });
