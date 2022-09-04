4d7ea923267d98f440b8ccce233e43ec5069d78b
Elkhan911 <955pro@mail.ru>
2022-09-04 19:22:43 +0500

    added win and lose func

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 const input1 = document.querySelector("#_input1");
 const span1 = document.querySelector("#_span1");
+const span2 = document.querySelector("#_span2");
+const span3 = document.querySelector("#_span3");
+const restartBtn = document.querySelector("#_restartBtn");
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const computerNumber = getRandom(0, 50);
+const computerNumber = getRandom(1, 50);
 console.log(computerNumber);
+
+let userMove = 0;
+
+function isItNaNOrEmpty(value) {
+  if (
+    isNaN(Number(value)) ||
+    value == "" ||
+    value == " " ||
+    value == "   " ||
+    value > 50
+  ) {
+    alert("Нужно ввести число. Число должны быть меньше 50");
+    return false;
+  } else value = "";
+  return true;
+}
 
 input1.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    if (input1.value > computerNumber) {
-      span1.textContent = "вы ввели значние БОЛЬШЕ";
-    }
+    if (isItNaNOrEmpty(this.value)) {
+      userMove++;
+      span2.textContent = userMove;
 
-    if (input1.value < computerNumber) {
-      span1.textContent = "вы ввели значние МЕНЬШЕ";
-    }
+      if (input1.value > computerNumber) {
+        span1.textContent = "вы ввели значние БОЛЬШЕ";
+      }
 
-    if (input1.value == computerNumber) {
-      span1.textContent = "БИНГО! Вы угадали";
+      if (input1.value < computerNumber) {
+        span1.textContent = "вы ввели значние МЕНЬШЕ";
+      }
+
+      if (input1.value == computerNumber) {
+        span1.textContent = "БИНГО! Вы победили";
+      }
     }
+    checkResult();
+    input1.value = "";
   }
 });
+
+function checkResult() {
+  if (span1.textContent.includes("БИНГО!")) {
+    span3.textContent == "Поздравляем, Вы одолели компьютер";
+    console.log(span1.textContent.includes("БИНГО!"));
+  }
+  if (Number(span2.textContent) > 10 && !span1.textContent.includes("БИНГО!")) {
+    span3.textContent = `В этот раз вы потерпели поражение. Правильный ответ был: ${computerNumber}`;
+  }
+}
